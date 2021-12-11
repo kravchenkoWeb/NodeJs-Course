51fb53cea5120693010dbca6864196e8118aaadb
Kravchenko <kv110696@gmail.com>
2021-12-11 17:35:26 +0200

    Lesson 2: added comment with process.exit()

--- Lesson/app.js
+++ Lesson/app.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 const reqListener = (req, res, next) => {
     console.log(req);
+    // Shut down server
+    // process.exit();
 };
 
 const server = http.createServer(reqListener);
